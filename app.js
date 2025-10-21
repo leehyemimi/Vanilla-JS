2c335d91f094d926906fcd2fc877939038a9ae87
이혜원 <sunone2@hunet.co.kr>
2025-10-21 18:51:48 +0900

    #Login

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-function plus(a,b){
-  console.log(a+b)
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function handleLoginFormSubmit(event){
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;1
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
 }
 
-function minus(a,b){
-  console.log(a-b)
+function paintGreeting(username){
+  greeting.innerText = `Welcome ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-plus(1,2);
-minus(1,3);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", handleLoginFormSubmit);
+}else{
+  paintGreeting(savedUsername);
+}
